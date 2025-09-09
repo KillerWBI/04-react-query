a9f5507ff220bee9891ca619f5cc4b614485de2f
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-09-09 15:15:06 +0200

    f

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
-
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import ReactPaginate from 'react-paginate';
 import { fetchMovies } from '../../services/movieService';
 import type { Movie } from '../../types/movie';
 import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
@@ -8,28 +9,27 @@ import { Loader } from '../Loader/Loader';
 import { MovieGrid } from '../MovieGrid/MovieGrid';
 import { MovieModal } from '../MovieModal/MovieModal';
 import { SearchBar } from '../SearchBar/SearchBar';
-import './App.module.css';
+import css from './App.module.css';
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(false);
   const [modalMovie, setModalMovie] = useState<Movie | null>(null);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const {data, isLoading, isError} = useQuery({
+    queryKey: ['movies', query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: !!query
+  });
+const movies = data?.results ?? [];
+const totalPages = data?.total_pages ?? 0;
+  const handleSearch = async (NewQuery: string) => {
+      if (!NewQuery.trim()) {
+        toast.error('Please enter your search query.');
+        return;
+      }
 
-  const [error, setError] = useState(false);
-
-
-  const handleSearch = async (query: string) => {
-    setMovies([]);
-    setError(false);
-    setLoading(true);
-    const result = await fetchMovies(query);
-    setLoading(false);
-    if (result.length === 0) {
-      toast.error("No movies found for your request.");
-      setError(true);
-      return;
-    }
-    setMovies(result);
+    setQuery(NewQuery);
+    setPage(1);
   };
 
   const handleSelect = (movie: Movie) => {
@@ -46,8 +46,20 @@ const closeModal = () => {
       <Toaster position="top-right" reverseOrder={false} />
       <SearchBar onSubmit={handleSearch} />
       <main>
-        {loading && <Loader />}
-        {error && <ErrorMessage />}
+        {isLoading && <Loader />}
+        {isError && <ErrorMessage />}
+        {totalPages > 1 && <ReactPaginate
+	    pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => setPage(selected + 1)}
+      forcePage={page - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+/>}
+
         {modalMovie && <MovieModal movie={modalMovie} onClose={closeModal}/>}
       <MovieGrid movies={movies} onSelect={handleSelect}/>
       </main>
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,23 +1,39 @@
 import axios from 'axios';
+import toast from 'react-hot-toast';
 import type { Movie } from '../types/movie';
+export interface MovieApiResponse {
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
 
-export async function fetchMovies(query: string): Promise<Movie[]> {
+export async function fetchMovies(query: string, page: number): Promise<MovieApiResponse> {
   try {
     const API_KEY = import.meta.env.VITE_API_KEY;
     const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-    const response = await axios.get<{ results: Movie[] }>(BASE_URL, {
-      params: { query },
+    const response = await axios.get<MovieApiResponse>(BASE_URL, {
+      params: {
+        query,
+        page
+
+      },
       headers: {
         accept: 'application/json',
         Authorization: `Bearer ${API_KEY}`,
       },
     });
-
-    return response.data.results;
+   if (!response.data.results.length) {
+      toast.error('No results found.');
+    }
+    return response.data;
   } catch (error) {
     console.error('Error fetching movies:', error);
-    return [];
+    return {
+      results: [],
+      total_pages: 0,
+      total_results: 0,
+    };
   }
 }
 export function getImageUrl(path: string): string {
